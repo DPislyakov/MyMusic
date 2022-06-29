ff9424d52af49527c6272d7a5c317074a67ccfb5
dmitrijpislakov <dpislyakov@bk.ru>
2022-06-29 21:57:53 +0200

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Дмитрий Писляков on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
